29c85cd19a0a5e0a2689ef4ebae384793a6afe08
snayrouz <samuel.nayrouz@gmail.com>
2017-10-10 08:52:19 -0600

    Add current user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   if Rails.env.development?
+    authenticate :user do
+
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
   end
 
   post "/graphql", to: "graphql#execute"
